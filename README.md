0e9bd0ab162139f825ced20fe7f1606e0b1a3782
ignore <ignore@email.address>
2021-08-23 14:38:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Linux RISC-V: development platformio example 
+
+Sample platform environment for use by [PlatformIO](http://platformio.org)
+
+# Usage
+
+```python
+pio platform install https://github.com/U2654/platform-linux_riscv.git
+```
+
---
+++ builder/main.py
@@ -0,0 +1,53 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os.path import join
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Builder, Default, DefaultEnvironment)
+
+env = DefaultEnvironment()
+
+env.Replace(
+    AR="riscv64-unknown-elf-gcc-ar",
+    AS="riscv64-unknown-elf-as",
+    CC="riscv64-unknown-elf-gcc",
+    GDB="gdb-multiarch",
+    CXX="riscv64-unknown-elf-g++",
+    OBJCOPY="riscv64-unknown-elf-objcopy",
+    RANLIB="riscv64-unknown-elf-gcc-ranlib",
+    SIZETOOL="riscv64-unknown-elf-size",
+
+    ARFLAGS=["rc"],
+
+    SIZEPRINTCMD='$SIZETOOL -d $SOURCES',
+
+    PROGSUFFIX=".elf",
+    PROGNAME="firmware"
+)
+
+#
+# Target: Build elf only
+#
+target_elf = env.BuildProgram()
+
+#
+# Target: Print binary size
+#
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Setup default targets
+#
+Default([target_elf, target_size])
